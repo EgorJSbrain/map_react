193f4a2360c95cb97b4ccf105012ab3f5ce289c3
Egor Pobylets <egor.pobylets@cogniteq.com>
2022-06-02 14:04:33 +0300

    feat: added styled-components

---
+++ src/utils/useCheckScreenSize.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export const useCheckScreenSize = () => {
+  const [screenSize, setScreenSize] = useState<number>(window.innerWidth);
+
+  const handleResize = () => {
+    if (window.innerWidth) {
+      setScreenSize(window.innerWidth);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
+
+  return screenSize;
+};
