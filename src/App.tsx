6a9fc5b29a127a2d33c9c31eef46e8bc38f635d8
Egor Pobylets <egor.pobylets@cogniteq.com>
2022-06-03 16:16:12 +0300

    feat: add i18n; fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { HomePage, Layout, SearchPage } from "./pages";
 
-const App = () => {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="" element={<HomePage />}/>
-          <Route path="search" element={<SearchPage />} />
-          <Route path="dashboard" />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="app" element={<HomePage />} />
+        <Route path="search" element={<SearchPage />} />
+        <Route path="dashboard" />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,56 @@
-import { NavLink } from "react-router-dom"
-import styled from "styled-components"
+import { NavLink, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import { useTranslation } from "react-i18next";
+import { Switch } from "@mui/material";
 
-const HeaderWrapper = styled.div`
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 62,
+  height: 34,
+  padding: 7,
+  '& .MuiSwitch-switchBase': {
+    margin: 1,
+    padding: 0,
+    transform: 'translateX(6px)',
+    '&.Mui-checked': {
+      color: '#fff',
+      transform: 'translateX(22px)',
+      '& .MuiSwitch-thumb:before': {
+        backgroundImage: `url(./uk.svg)`,
+      },
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: '#ffe108',
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    backgroundColor: '#ffe108',
+    width: 32,
+    height: 32,
+    '&:before': {
+      content: "''",
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      left: 0,
+      top: 0,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundImage: `url(./ru.svg)`,
+    },
+  },
+  '& .MuiSwitch-track': {
+    opacity: 1,
+    backgroundColor: 'red',
+    borderRadius: 20 / 2,
+  },
+}));
+
+const HeaderWrapper = styled.header<{isApp: boolean}>`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #59ddff;
+  background-color: ${props => (props.isApp ? `transparent` : `#59ddff`)};;
   padding: 24px;
   position: absolute;
   top: 0;
@@ -20,9 +65,9 @@ const Links = styled.div`
   align-items: center;
 `
 
-const Link = styled(NavLink)`
+const Link = styled(NavLink)<{isApp: boolean}>`
   text-decoration: inherit;
-  color: #395964;
+  color: ${props => props.isApp ? '#88f1ff' : '#395964'};
 
   :hover {
     color: white;
@@ -33,14 +78,34 @@ const Link = styled(NavLink)`
   }
 `
 
+const LangBlock = styled.div`
+  position: absolute;
+  right: 0;
+`
+
 export const Header = () => {
+  const { t, i18n } = useTranslation();
+  const location = useLocation();
+  const isApp = location.pathname.includes('app')
+
+  const changeLanguage = (checked: boolean) => {
+    i18n.changeLanguage(checked ? 'en' : 'ru');
+  };
+
   return (
-    <HeaderWrapper>
+    <HeaderWrapper isApp={isApp}>
       <Links>
-        <Link to="/">HOME</Link>
-        <Link to="/search">MAP</Link>
-        <Link to="/dashboard">CARDS</Link>
+        <Link to="/app" isApp={isApp}>{t('navHome')}</Link>
+        <Link to="/search" isApp={isApp}>{t('navMap')}</Link>
+        <Link to="/dashboard" isApp={isApp}>{t('navCards')}</Link>
       </Links>
+      <LangBlock>
+        <MaterialUISwitch
+          sx={{ m: 1 }}
+          defaultChecked
+          onChange={(e, checked) => changeLanguage(checked)}
+        />
+      </LangBlock>
     </HeaderWrapper>
   );
 }
