10377ff8342736057a624d7cd2af9da3abf3c31b
Egor Pobylets <egor.pobylets@cogniteq.com>
2022-06-13 16:34:17 +0300

    feat: add tests;

---
+++ src/components/Header/Navbar.test.js
@@ -0,0 +1,69 @@
+import { render, screen } from "@testing-library/react"
+import { Navbar } from "./Navbar"
+import { MemoryRouter } from 'react-router-dom'
+import userEvent from "@testing-library/user-event";
+import { App } from '../../App'
+import { Provider } from 'react-redux';
+import { store } from "../../store/store";
+import * as redux from 'react-redux';
+
+const routerRender = () => {
+  render(
+    <MemoryRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </MemoryRouter>
+  );
+}
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => {
+    return {
+      t: (str) => str,
+    };
+  },
+}));
+
+jest.mock('react-redux', () => {
+  const ActualReactRedux = jest.requireActual('react-redux');
+  return {
+      ...ActualReactRedux,
+      useSelector: jest.fn().mockImplementation(() => {
+          return mockState;
+      }),
+  };
+});
+
+describe("HEADER TEST", () => {
+  let spyOnUseSelector;
+
+  beforeEach(() => {
+    spyOnUseSelector = jest.spyOn(redux, 'useSelector');
+    spyOnUseSelector.mockReturnValue({ id: 1 });
+  });
+
+  test("render links", () => {
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    );
+  });
+
+  test('test home link', () => {
+    routerRender();
+
+    const homeLink = screen.getByTestId('home-link')
+    userEvent.click(homeLink);
+    expect(screen.getByTestId('home-page')).toBeInTheDocument;
+  })
+
+  test('test search link', () => {
+    routerRender();
+
+    const homeLink = screen.getByTestId('search-link')
+    userEvent.click(homeLink);
+    expect(screen.getByTestId('search-page')).toBeInTheDocument;
+  })
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,26 @@
 import { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import {App} from "./App";
 
 import "./i18n";
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <Suspense fallback={<div>Loading...</div>}>
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ThemeProvider>
-  </Suspense>
+  <BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ThemeProvider>
+    </Suspense>
+  </BrowserRouter>
 );
